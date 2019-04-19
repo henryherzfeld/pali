7db5094c6641d5908d0e2d60be72e8f5861a6078
Henry Herzfeld <herzfeld2@gmail.com>
2019-04-19 20:18:47 +0000

    finalize algorithms

--- alg/bf_pali.py
+++ alg/bf_pali.py
@@ -4,31 +4,43 @@ logger = logging.getLogger(__name__)
 
 def pali(word):
 
+    # initialization
     n = len(word)
-    if n == 0:
-        return 0
-
-    elif n == 1:
-        return word
-
     i_start = 0
     i_end = 0
     max_end = 0
     max_start = 0
 
+    # handle small input strings
+    if n == 0:
+        return 0
+    if n == 1:
+        return word
 
+    # move start index through entire word
     while i_start <= n:
         i_end = i_start + 1
+
+        # move end index through entire word
         while i_end < n:
+
+            # test current substring
             if is_pali(word, i_start, i_end):
+
+                # if palindrome size is greater than max palindrome
+                # assign palindrome size to max palindrome
                 if i_end - i_start >= max_end - max_start:
                     max_start, max_end = i_start, i_end
+
+        # loop iterators
             i_end = i_end + 1
         i_start = i_start + 1
 
     logger.info(word[max_start:max_end+1])
     return word[max_start:max_end+1]
 
+# employs "onion" or peel method to test palindrome within
+# given start and end indices
 def is_pali(word, i_start, i_end):
     i = i_end
 
